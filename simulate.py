cf1f3839661be1a6036d874a46517210fcc48d5e
Brendon J. Brewer <brendon.brewer@gmail.com>
2020-10-07 12:53:22 +1300

    some work

--- simulate.py
+++ simulate.py
@@ -7,6 +7,12 @@ NUM_CHANNELS = 4
 def phi(x):
     return np.log10(phi + 10.0)
 
+def update(y, w):
+    """
+    One iteration of the procedure to compute the ys
+    """
+    ynew = 0.0
+
 # Matrix of supports. Start with diagonal
 w = np.zeros((NUM_CHANNELS, NUM_CHANNELS))
 w += np.diag(np.exp(3.0 + rng.randn(NUM_CHANNELS)))
@@ -17,4 +23,14 @@ for r in range(3):
     j = rng.randint(NUM_CHANNELS)
     w[i, j] += np.exp(3.0*rng.randn())
 
+print("Support matrix:")
+print("---------------", end="\n\n")
+print(w, end="\n\n")
+
+# Credibilities
+y = np.ones(NUM_CHANNELS)
+print("After 0 iterations:", y)
+for i in range(10):
+    update(y, w)
+    print(f"After {i+1} iterations:", y)
 
